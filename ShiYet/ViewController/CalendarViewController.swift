aa5f7070e230b38e489a450788bff21e49ac5a9c
Hao Jiang <108918757+JackyJiang0410@users.noreply.github.com>
2023-09-23 10:13:18 -0500

    Add files via upload

---
+++ ShiYet/ViewController/CalendarViewController.swift
@@ -0,0 +1,23 @@
+//
+//  CalendarViewController.swift
+//  ShiYet
+//
+//  Created by 江昊 on 9/23/23.
+//
+
+import Foundation
+
+import UIKit
+
+class CalendarViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func test(_ sender: Any) {
+        view.backgroundColor = .black
+    }
+}
---
+++ ShiYet/ViewController/HisStatViewController.swift
@@ -0,0 +1,23 @@
+//
+//  HisStatViewController.swift
+//  ShiYet
+//
+//  Created by 江昊 on 9/23/23.
+//
+
+import Foundation
+
+import UIKit
+
+class HisStatViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+    @IBAction func test(_ sender: Any) {
+        view.backgroundColor = .black
+    }
+}
