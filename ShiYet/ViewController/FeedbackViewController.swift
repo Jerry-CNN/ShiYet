1e1e83dfb6d703be1fa3c04a7d403e8131158b02
JackyJiang0410 <hj37@rice.edu>
2023-09-23 15:49:04 -0500

    structure

--- ShiYet/ViewController/FeedbackViewController.swift
+++ ShiYet/ViewController/FeedbackViewController.swift
@@ -3,13 +3,29 @@ import Foundation
 import UIKit
 
 class FeedbackViewController: UIViewController {
+    
+    var submitted = false
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-    @IBAction func Quit(_ sender: Any) {
-        dismiss(animated: true, completion: nil)
+
+    @IBAction func submit(_ sender: Any) {
+        if !submitted {
+            let reportVC = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "ReportViewController") as! ReportViewController
+
+            // Present the FeedbackViewController
+            self.present(reportVC, animated: true, completion: nil)
+            submitted = true
+            (sender as AnyObject).setTitle("Quit", for: .normal)
+        }
+        else {
+            self.dismiss(animated: true, completion: nil)
+        }
+        
     }
+    
+
 }
