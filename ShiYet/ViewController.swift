c7bd2cda25be3fafd864f8d458314b81ec9f3c74
jerrry <Jerrycaiyx@hotmail.com>
2023-09-23 00:38:20 -0500

    Initial Commit

---
+++ ShiYet/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShiYet
+//
+//  Created by 蔡延校 on 9/23/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ ShiYetUITests/ShiYetUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  ShiYetUITestsLaunchTests.swift
+//  ShiYetUITests
+//
+//  Created by 蔡延校 on 9/23/23.
+//
+
+import XCTest
+
+class ShiYetUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
