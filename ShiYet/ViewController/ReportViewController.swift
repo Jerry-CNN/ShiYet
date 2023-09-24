f37e627897f7f6210bb28e63090c1f9037369a0a
jww829 <jw143@rice.edu>
2023-09-23 20:11:24 -0500

    ReportView

--- ShiYet/ViewController/ReportViewController.swift
+++ ShiYet/ViewController/ReportViewController.swift
@@ -1,46 +1,65 @@
-import Foundation
-
 import UIKit
 
 class ReportViewController: UIViewController {
 
+    let contentView = UIView()
+
+    @IBOutlet weak var ReportSection: UIScrollView!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         setupScrollView()
-    }
 
+        // Add content to contentView (this can be any subviews you want)
+        let contentLabel = UILabel()
+        contentLabel.text = "Your vertically scrollable content goes here. nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnYou can add more labels, images, or other views as needed.Your vertically scrollable content goes here. nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnYou can add more labels, images, or other views as needed.Your vertically scrollable content goes here. nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnYou can add more labels, images, or other views as needed.Your vertically scrollable content goes here. nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnYou can add more labels, images, or other views as needed.Your vertically scrollable content goes here. nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnYou can add more labels, images, or other views as needed.Your vertically scrollable content goes here. nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnYou can add more labels, images, or other views as needed.Your vertically scrollable content goes here. nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnYou can add more labels, images, or other views as needed.Your vertically scrollable content goes here. nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnYou can add more labels, images, or other views as needed.Your vertically scrollable content goes here. nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnYou can add more labels, images, or other views as needed.Your vertically scrollable content goes here. nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnYou can add more labels, images, or other views as needed.Your vertically scrollable content goes here. nnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnYou can add more labels, images, or other views as needed."
+        contentLabel.numberOfLines = 0 // Allow multiple lines of text
+        contentView.addSubview(contentLabel)
+
+        // Add constraints for the contentLabel within contentView
+        contentLabel.translatesAutoresizingMaskIntoConstraints = false
+        contentLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+        contentLabel.topAnchor.constraint(equalTo: contentView.topAnchor, constant: 20).isActive = true
+        contentLabel.leadingAnchor.constraint(equalTo: contentView.leadingAnchor, constant: 20).isActive = true
+        contentLabel.trailingAnchor.constraint(equalTo: contentView.trailingAnchor, constant: -20).isActive = true
+        contentLabel.bottomAnchor.constraint(equalTo: contentView.bottomAnchor, constant: -20).isActive = true
+        contentLabel.baselineAdjustment = .alignCenters
+        
+        contentLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor).isActive = true
+
+    }
 
     @IBAction func Back(_ sender: Any) {
         self.dismiss(animated: true, completion: nil)
     }
-    
-    let contentView = UIView()
-    
-    @IBOutlet weak var ReportSection: UIScrollView!
-    
+
     func setupScrollView() {
-        // 将滚动视图添加到视图控制器的视图中
+        // Add ReportSection (UIScrollView) to the view controller's view
         view.addSubview(ReportSection)
-        
-        // 将内容视图添加到滚动视图中
+
+        // Add contentView to ReportSection (UIScrollView)
         ReportSection.addSubview(contentView)
-        
-        // 启用横向滚动
+
+        // Enable vertical scrolling
         ReportSection.isScrollEnabled = true
-        
-        // 设置内容视图的背景色（这只是为了示例）
-        contentView.backgroundColor = UIColor.red
-        
-        // 添加内容视图的约束，使其与滚动视图大小相同，横向滚动
+
+        // Set contentView's background color (just for illustration)
+        contentView.backgroundColor = UIColor.systemBackground
+
+        // Add constraints for contentView
         contentView.translatesAutoresizingMaskIntoConstraints = false
         contentView.topAnchor.constraint(equalTo: ReportSection.topAnchor).isActive = true
         contentView.leadingAnchor.constraint(equalTo: ReportSection.leadingAnchor).isActive = true
         contentView.bottomAnchor.constraint(equalTo: ReportSection.bottomAnchor).isActive = true
         contentView.trailingAnchor.constraint(equalTo: ReportSection.trailingAnchor).isActive = true
-        contentView.heightAnchor.constraint(equalTo: ReportSection.heightAnchor).isActive = true
-        
-        // 设置内容视图的宽度，这将决定横向滚动的范围
-        contentView.widthAnchor.constraint(equalTo: ReportSection.widthAnchor, multiplier: 2.0).isActive = true
-    }
 
+        // Set the width constraint to control horizontal scrolling
+        contentView.widthAnchor.constraint(equalTo: ReportSection.widthAnchor).isActive = true
+
+        // Add a height constraint to control vertical scrolling
+        contentView.heightAnchor.constraint(greaterThanOrEqualTo: ReportSection.heightAnchor).isActive = true
+        
+
+    }
+    
 }
